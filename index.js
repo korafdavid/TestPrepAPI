5047ab1d8d0bb391cfc9463a0aa548893dd532a1
David Okoroafor <okoroafordavid61@gmail.com>
2021-08-15 21:23:33 +0100

    displays all data format

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-var PORT = process.env.PORT  || 8080;
+const path = require('path');
+var PORT = process.env.PORT || 8080;
+var contentType = 'application/json';
 
-var server = http.createServer((req,res)=>{
-      var e = url.parse(req.url,true);
-   var filename = '.'+ e.pathname;
-    fs.readFile(filename,'utf8',(err,data) => {
-     res.writeHead(201,{'Content-Type':'application/json'});
-     if (err) {
-       res.writeHead(404, {'Content-Type':'Text/HTML'});
-         return res.end('error 404 not Found ' + err);
-     }
-     var ref = JSON.parse(JSON.stringify(data));
-     res.end(ref, typeof decoded);
-    });
+
+var server = http.createServer((req, res) => {
+  var e = url.parse(req.url, true);
+  var filename = '.' + e.pathname;
+  var extensionName = path.extname(filename);
+  switch (extensionName) {
+    case '.mp3':
+      contentType = 'audio/mpeg';
+      break;
+    case '.gif':
+      contentType = 'image/gif';
+      break;
+    case '.jpeg':
+      contentType = 'image/jpeg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.js':
+      contentType = 'test/javascript';
+      break;
+    case '.mp4':
+      contentType = "video/mp4";
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;  
+  }
+  console.log(contentType);
+  var encoding = contentType == 'application/json' ? 'UTF-8' : '';
+  fs.readFile(filename, encoding, (err, data) => {
+    res.writeHead(200, {'Content-Type': contentType});
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'Text/HTML'});
+      return res.end('error 404 not Found ' + err);
+    }
+    if (contentType == 'application/json') {
+       var ref = JSON.parse(JSON.stringify(data));
+       res.end(ref, typeof decoded);
+     } else{
+    res.end(data);
+    }
+  });
 });
-server.listen(PORT, ()=>{console.log(`server running on ${PORT} ...`)});
+server.listen(PORT, () => { console.log(`server running on ${PORT} ...`) });
+
+
+
+var mime = {
+  html: 'text/html',
+  txt: 'text/plain',
+  css: 'text/css',
+  gif: 'image/gif',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  js: 'application/javascript'
+};
+  //const stat = fs.statSync(filePath);
